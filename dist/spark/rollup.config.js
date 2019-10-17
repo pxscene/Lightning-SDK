8d3d56996a4dd54551f00f8bc99cc929e9b8f7a5
Nikita Poltorapavlo <Nikita_Poltorapavlo2@cable.comcast.com>
2019-10-17 19:06:55 +0300

    clean skeleton rollup

--- dist/spark/rollup.config.js
+++ dist/spark/rollup.config.js
@@ -1,10 +1,5 @@
-const resolve = require('rollup-plugin-node-resolve');
-
 export default {
     input: 'start.mjs',
-    plugins: [resolve({
-        only: [ 'wpe-lightning-spark', 'wpe-lightning' ]
-    })],
     output: {
         file: './lightning-demo-spark.js',
         format: 'cjs',
